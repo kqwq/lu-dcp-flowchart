64d2b40bf4112b7764fa5be7d7a440edbf2e3f2b
Kyle <kyle@example.com>
2023-02-07 20:51:40 -0500

    improvement

--- tests/parseDcpPdf.js
+++ tests/parseDcpPdf.js
@@ -5,7 +5,7 @@
 // A new file ./tests/pdf.json will be created when a file with contents that match exactly "quit" is saved
 // Done
 
-import fs from "fs";
+import fs, { write } from "fs";
 
 const degrees = [];
 
@@ -49,12 +49,49 @@ function extractCourseNumbers(text) {
   return matchesArray;
 }
 
+function writeDegreesToFile(filename) {
+  // Replace "(\w{4})\s(\d{3})" with "c.(\1\2)"
+  const output = JSON.stringify(degrees, null, 2);
+  output.replace(/'(\w{4})\s(\d{3})'/gm, "c.$1$2");
+  fs.writeFileSync(filename, output);
+}
+
+function inputCommand(input) {
+  let firstWord = input.split(" ")[0];
+  let restOfInput = input.slice(input.indexOf(" "));
+  console.log(firstWord, restOfInput);
+
+  // If single-word input as file, do a special action
+  switch (firstWord) {
+    case "quit":
+      console.log("Exiting");
+      process.exit(0);
+      break;
+
+    case "save":
+      writeDegreesToFile(restOfInput);
+      console.log("Wrote to file " + restOfInput);
+      break;
+
+    case "pop":
+      degrees.pop();
+      console.log("Removed last item from degrees");
+      break;
+  }
+}
+
 // Listen for changes to the file "./tests/pdf.txt"
-fs.readFile(watchFilename, (curr, prev) => {
+fs.watchFile(watchFilename, (curr, prev) => {
   // Extract all instances of "wwww ddd" from the file contents
   // allText is the entire text of the PDF (2 pages)
   // text is only the first page MINUS the "Notes" section AND the "Graduation Requirements" section
   const allText = fs.readFileSync(watchFilename).toString();
+
+  if (allText.split("\n").length === 1) {
+    inputCommand(allText);
+    return;
+  }
+
   const text = allText.slice(0, allText.indexOf("Notes"));
   const major = {};
 
@@ -63,12 +100,13 @@ fs.readFile(watchFilename, (curr, prev) => {
   let nonGenEdSections = sections.filter(
     (section) => !generalEdSections.includes(section)
   );
+  console.log("nonGenEdSections", nonGenEdSections);
 
   // Divide into indices of each section
   let indicesOfSections = [];
   for (let i = 0; i < nonGenEdSections.length; i++) {
     let section = nonGenEdSections[i];
-    let index = text.indexOf(section);
+    let index = text.indexOf(section + " (");
     indicesOfSections.push(index);
     // Add to degrees
     major[section] = [];
@@ -97,17 +135,27 @@ fs.readFile(watchFilename, (curr, prev) => {
   // Exract date revised (get group 1, 2, and 3)
   const textLines = text.split("\n");
   let dateMMDDYYYY = textLines[0]
-    .match(/Revised:\s+(\d+)\.(\d+)\.(\d+)/g)
-    .slice(0, 3);
-  let dateRevised = new Date(dateMMDDYYYY[2], dateMMDDYYYY[1], dateMMDDYYYY[0]);
+    .match(/Revised:\s+(\d+)\.(\d+)\.(\d+)/)
+    .slice(1, 4);
+
+  console.log(dateMMDDYYYY);
+  let dateRevised = new Date(
+    dateMMDDYYYY[2],
+    dateMMDDYYYY[0] - 1,
+    dateMMDDYYYY[1]
+  );
 
   // Extract total hours (get group 1)
-  let totalHours = text.match(/(\d+)\sTotal\sHours/)[1];
+  let totalHours = parseInt(allText.match(/(\d+)\sTotal\sHours/)[1]);
+  console.log(totalHours);
 
   // Generate JSON output
   let degree = {
     name: textLines[1].trim(),
-    year: textLines[2].split(" ")[0],
+    year: textLines
+      .slice(2, 4)
+      .join(" ")
+      .match(/\d{4}-\d{4}/)[0],
     dateRevised: dateRevised,
     dateEffective: textLines[0].split("Effective: ")[1].trim(),
     totalHours: totalHours,
@@ -115,9 +163,6 @@ fs.readFile(watchFilename, (curr, prev) => {
   };
   console.log(1, degree);
   degrees.push(degree);
-
-  // Replace "(\w{4})\s(\d{3})" with "c.(\1\2)"
-  // output.replace(/'(\w{4})\s(\d{3})'/gm, "c.$1$2");
 
   // console.log(7, output);
 });
