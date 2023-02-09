6a542f7a1d48dd96d3e8e44fe63c522d59ca27ec
Kyle W <kyle@example.com>
2023-02-09 08:52:16 -0500

    a

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 import Navbar from './Navbar'
 
@@ -13,16 +13,28 @@ const Home = () => {
 
 
   return (
-    <>
-      <Navbar />
-      <Container mt="65px" textAlign={"left"}>
-        <Heading size="lg">LU DCP Flowchart Maker</Heading>
-        <Text>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi voluptates sit recusandae natus ea nam quisquam, praesentium dolores minus ut eaque fugiat ad, molestiae cupiditate, porro temporibus aspernatur perspiciatis magni.</Text>
-
-        {/* Three dropdown inputs: Program year, school, degree program */}
-
+    <Box h="100vh">
+      // Vertically center the content
+      <Container maxW="container.xl" h="100%" display="flex" alignItems="center">
+        <Box>
+          <Heading as="h1" size="4xl" mb="4">
+            Welcome to the world of <br /> <Text as="span" color="blue.500">Chakra UI</Text>
+          </Heading>
+          <Text fontSize="xl" mb="8">
+            Chakra UI is a simple, modular and accessible component library that gives you the building blocks you need to build your React applications.
+          </Text>
+          <Text fontSize="xl" mb="8">
+            Here are some of the countries in the world:
+          </Text>
+          <ul>
+            {countries.map((country) => (
+              <li key={country}>{country}</li>
+            ))}
+          </ul>
+        </Box>
       </Container>
-    </>
+
+    </Box>
   )
 }
 
