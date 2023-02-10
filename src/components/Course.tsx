b0caee5d6fcb323664a812bfb6839ccb96e0d5fa
K W <kylew864@gmail.com>
2023-02-10 09:40:39 -0500

    pill shaped buttons

--- src/components/Course.tsx
+++ src/components/Course.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Button, Box, Text } from '@chakra-ui/react'
+import { Button, Box, Text, Badge, Tooltip } from '@chakra-ui/react'
 
 const Course = ({ courseObj, clickedCallback }) => {
-
+  const [isSelected, setIsSelected] = React.useState(false)
   // Pill-shaped button that 
 
+  const onClick = () => {
+    setIsSelected(!isSelected)
+    clickedCallback(courseObj)
+  }
+
   return (
-    <Button onClick={() => { clickedCallback(courseObj) }} colorScheme="purple" size="sm"  >{courseObj?.code || courseObj.join(" or ")}</Button>
+    <Tooltip label={courseObj?.title} aria-label={courseObj?.title} placement="top">
+      <Button variant={
+        isSelected ? "solid" : "outline"
+
+      }
+        borderRadius="full" onClick={onClick} colorScheme="purple" size="sm"  >{courseObj?.code || courseObj.join(" or ")}</Button>
+    </Tooltip>
 
 
   )
