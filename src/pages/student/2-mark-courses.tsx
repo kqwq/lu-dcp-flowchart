ad3489f2024d026223f46306e6b205389dbcb78e
Kyle <kyle@example.com>
2023-02-09 21:58:12 -0500

    a

--- src/pages/student/2-mark-courses.tsx
+++ src/pages/student/2-mark-courses.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Box, Button, Container, Heading, InputGroup, Text, VStack, Flex } from '@chakra-ui/react'
+import CourseSelectionInput from '../../components/CourseSelectionInput'
+import { useRouter } from 'next/router'
+
 
 const Page = () => {
+
+  const router = useRouter()
   return (
-    <div>Page</div>
+    <>
+      <Flex h="calc(100vh - 60px)">
+        <Flex justify="center" align="center" w="full"  >
+          <VStack spacing={10} >
+            <Box w="100%">
+              <Heading as="h1" size="2xl" mb="4" >Select a major</Heading>
+            </Box>
+
+            <CourseSelectionInput />
+
+            <Flex w="100%" justify="right">
+              <Button colorScheme="blue" size="lg" onClick={() => {
+                router.push('/student/2-mark-courses')
+              }}>Next</Button>
+            </Flex>
+          </VStack>
+
+        </Flex>
+      </Flex>
+    </>
   )
 }
 
